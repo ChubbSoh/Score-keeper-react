540b3f39d861cebf82c7d228bd3343ea5335db35
Loo Jye Miin <loojyemiin@gmail.com>
2018-12-24 11:53:41 +0800

    updated bottm navbar

--- src/components/bottom-nav.js
+++ src/components/bottom-nav.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const BottomBar = styled.div`
-    width: 100%;
+const HomeButton = styled.button`
+    width: 50%;
     height: 56px;
     background: #CBCBCB;
     position: fixed;
+    left: 0;
     bottom: 0;
+    border: none;
 `;
+
+const GameButton = styled.button`
+    width: 50%;
+    height: 56px;
+    background: #CBCBCB;
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    border: none;
+`;
+
 
 export default class BottomNav extends Component {
     render() {
         return (
-            <BottomBar>
-            </BottomBar>
+            <div>
+                <HomeButton></HomeButton>
+                <GameButton></GameButton>
+            </div>
         )
     }
 }
