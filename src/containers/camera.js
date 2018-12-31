f3382bc73c62b2eed43a1147326202b15d17ea1e
fu_lim <fulim_88@yahoo.com>
2018-12-31 11:24:50 +0800

    face++ api

--- src/containers/camera.js
+++ src/containers/camera.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
 
 const CameraButton = styled.label`
     width: 250px;
@@ -55,6 +56,53 @@ const CameraIcon = styled.img`
 `;
 
 export default class Camera extends Component {
+    state = {
+        file: null,
+        img: null,
+        result: [],
+        imgHeight: 0,
+        imgWidth: 0,
+    }
+
+    handleInput = e => {
+        const file = e.target.files[0]
+        const img = new Image()
+        img.src = URL.createObjectURL(file)
+        img.onload = () => {
+            this.setState({
+              imgHeight: img.height,
+              imgWidth: img.width
+            })
+        }
+
+        this.setState({
+            file,
+            img: img.src
+        })
+        this.callAPI(file)
+    }
+
+    callAPI = (file) => {
+        const bodyFormData = new FormData();
+        bodyFormData.append('image_file', file)
+        Axios({
+            url: "https://api-us.faceplusplus.com/facepp/v3/detect",
+            method: "POST",
+            params: {
+                api_key: 'n1-3x1J8wJkWdHHftOMNh0Xiob038l0t',
+                api_secret: 'QODWghePwyL3vCuBdmFaAuSdxuvwVcHQ',
+            },
+            data: bodyFormData
+        })
+        .then(res => {
+            console.log(res.data)
+            console.log(res.data.faces)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
     render() {
         return (
             <div>
@@ -62,12 +110,8 @@ export default class Camera extends Component {
                     <Header>Add players</Header>
                     <br />
                     <InnerContainer>
-                        <CameraButton>
-                            <div>
-                                <CameraIcon src='icon/camera-icon.svg' />
-                                Take a wefie!
-                            </div>
-                            <Input type="file" accept="image/*" capture="user" />
+                        <CameraButton>Take a wefie!
+                                <Input onChange={this.handleInput} type="file" accept="image/*" capture="user" />
                         </CameraButton>
                     </InnerContainer>
                     <br />
