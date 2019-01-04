0f8887e5893b177e7b254741d8fe4a207a5b5262
Liren <liren90@gmail.com>
2019-01-04 17:16:14 +0800

    Updates

---
+++ precache-manifest.bb11a6d19317a3d99e9ce50b286237bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9ba00b4aba5598351799f92847585f9",
+    "url": "/sc0re/static/media/loading2.f9ba00b4.gif"
+  },
+  {
+    "revision": "ce09718b52580e2d730f",
+    "url": "/sc0re/static/js/runtime~main.ce09718b.js"
+  },
+  {
+    "revision": "bb65b071746dcfecf926",
+    "url": "/sc0re/static/js/main.bb65b071.chunk.js"
+  },
+  {
+    "revision": "174d97132259ccea1d59",
+    "url": "/sc0re/static/js/1.174d9713.chunk.js"
+  },
+  {
+    "revision": "bb65b071746dcfecf926",
+    "url": "/sc0re/static/css/main.58b8d571.chunk.css"
+  },
+  {
+    "revision": "174d97132259ccea1d59",
+    "url": "/sc0re/static/css/1.4cbfa51a.chunk.css"
+  },
+  {
+    "revision": "ac48e663424e508d8427201f790892da",
+    "url": "/sc0re/index.html"
+  }
+];
